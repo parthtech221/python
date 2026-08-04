b145be8ee89252ecaf2c23b881d414615c415667
parthtech221 <parthtech221@gmail.com>
2026-08-04 10:55:39 +0530

    Add files via upload

---
+++ 009.py
@@ -0,0 +1,5 @@
+p=float(input("enter the principal amount: "))
+r=float(input("enter the rate of interest: "))
+t=float(input("enter the time period: "))
+i=(p*r*t)/100
+print("the simple interest is:", i)
