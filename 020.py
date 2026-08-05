87548425ef929c08b066b54ddbbc3f5072373d87
parthtech221 <parthtech221@gmail.com>
2026-08-05 10:31:11 +0530

    Add files via upload

---
+++ 020.py
@@ -0,0 +1,10 @@
+#login system
+username = input("Enter username: ")
+password = input("Enter password: ")
+
+if username == "admin" and password == "password123":
+    print("Login successful!")
+else:
+    print("Login failed. Please check your username and password.")
+    
+    
