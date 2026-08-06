9328350086b4b501320b1326697bb1c755bf2225
parthtech221 <parthtech221@gmail.com>
2026-08-06 10:40:02 +0530

    Add files via upload

---
+++ 037.py
@@ -0,0 +1,10 @@
+a=int(input("Enter a number: "))
+count=0
+
+while a>0:
+    digit=a%10
+    a//=10
+    if digit%2==0:
+        count+=1
+    
+print(f"The number of even digits in the number is {count}.")    
