3791be530cdea5f898c43080976b8d865dd66674
parthtech221 <parthtech221@gmail.com>
2026-08-11 18:24:02 +0530

    Add files via upload

---
+++ 085.py
@@ -0,0 +1,5 @@
+#count occurrence of a character in a string
+string = input("Enter a string: ")
+char = input("Enter the character to count: ")
+count = string.count(char)
+print(f"The character '{char}' occurs {count} times in the string.")
