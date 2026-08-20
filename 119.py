a206a19482da4a5c6e58e43fba74c0c1e63e796d
parthtech221 <parthtech221@gmail.com>
2026-08-20 16:27:47 +0530

    Add files via upload

--- 119.py
+++ 119.py
@@ -1,9 +1,11 @@
 def selection_sort(lst):
     n=len(lst)
     for i in range(n):
-        for j in range(n):
-            if lst[i]<lst[j]:
-                lst[j], lst[i] = lst[i], lst[j]
+        min_idx = i
+        for j in range(i + 1, n):
+            if lst[j] < lst[min_idx]:
+                min_idx = j
+        lst[i], lst[min_idx] = lst[min_idx], lst[i]
     return lst
 
 my_list = [10, 2, 3, 6, 4, 9, 5]
