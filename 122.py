8dc2d13a4946d2a38ec796ad8a6fef814982d75e
parthtech221 <parthtech221@gmail.com>
2026-08-20 16:30:01 +0530

    Add files via upload

---
+++ 122.py
@@ -0,0 +1,20 @@
+def binarysearch(arr, left, right, target):
+
+    if left > right:
+        return -1
+
+    mid = (left + right) // 2
+
+    if arr[mid] == target:
+        return mid
+
+    elif target > arr[mid]:
+        return binarysearch(arr, mid + 1, right, target)
+
+    else:
+        return binarysearch(arr, left, mid - 1, target)
+
+
+arr = [1, 2, 3, 4, 5, 6, 7]
+
+print(binarysearch(arr, 0, len(arr)-1, 5))
